6b8f4d605d572413cd8e14bd95ae2f51215a4788
Suchitra Guduru <72724857+suchitraguduru@users.noreply.github.com>
2023-06-05 09:53:11 -0700

    Update EditDistance.java

--- 25_DynamicProgramming/EditDistance.java
+++ 25_DynamicProgramming/EditDistance.java
@@ -1,1 +1,15 @@
-
+public static int editDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m+1][n+1];
+        return func(m,n,word1,word2,dp);
+    }
+    public static int func(int i,int j,String s1,String s2,int[][] dp){
+        if(i==0) return dp[i][j]=j;
+        if(j==0) return dp[i][j]=i;
+        if(dp[i][j]!=0){
+            return dp[i][j];
+        }
+            if(s1.charAt(i-1)==s2.charAt(j-1)){ return dp[i][j]=func(i-1,j-1,s1,s2,dp);}
+        return dp[i][j]=1+Math.min(func(i,j-1,s1,s2,dp),Math.min(func(i-1,j,s1,s2,dp),func(i-1,j-1,s1,s2,dp)));
+    }
