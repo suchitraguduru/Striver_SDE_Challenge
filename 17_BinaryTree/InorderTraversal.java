6dd1c94b9797f78960d4f74ebd9401c1e8ebc793
Suchitra Guduru <72724857+suchitraguduru@users.noreply.github.com>
2023-06-05 08:23:27 -0700

    Update InorderTraversal.java

--- 17_BinaryTree/InorderTraversal.java
+++ 17_BinaryTree/InorderTraversal.java
@@ -1,1 +1,13 @@
-
+public static List < Integer > getInOrderTraversal(TreeNode root) {
+        ArrayList<Integer> hash = new ArrayList<>();
+        inOrder(root,hash);
+        return hash;
+    }
+    public static void inOrder(TreeNode root,ArrayList<Integer> hash){
+        if(root==null){
+            return;
+        }
+        inOrder(root.left,hash);
+        hash.add(root.data);
+        inOrder(root.right,hash);
+    }
