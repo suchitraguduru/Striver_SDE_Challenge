92892dda03ef9a258ff6167494ecadb39ca1f854
Suchitra Guduru <72724857+suchitraguduru@users.noreply.github.com>
2023-06-09 19:51:42 -0700

    Update TwoSum.java

--- 04_Arrays/TwoSum.java
+++ 04_Arrays/TwoSum.java
@@ -1,1 +1,13 @@
-
+public static List<int[]> pairSum(int[] arr, int s) {
+       List<int[]> res = new ArrayList<>();
+       Arrays.sort(arr);
+       for(int i=0;i<arr.length;i++){
+           for(int j=i+1;j<arr.length;j++){
+               if(arr[i]+arr[j]==s){
+                res.add(new int[]{arr[i],arr[j]});
+              }
+           }
+           
+       }
+       return res;
+}
