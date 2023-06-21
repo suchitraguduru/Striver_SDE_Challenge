5833b93ab772b634681ba4cd770eacc9158a8379
Suchitra Guduru <72724857+suchitraguduru@users.noreply.github.com>
2023-06-21 02:26:52 -0700

    Update DetectCycle.java

--- 06_LinkedList/DetectCycle.java
+++ 06_LinkedList/DetectCycle.java
@@ -1,1 +1,10 @@
-
+public static boolean detectCycle(Node head) {
+        Node fast = head;
+        Node slow = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast==slow)return true;
+        }
+        return false;
+    }
